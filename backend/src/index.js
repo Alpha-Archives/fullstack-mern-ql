6dedbf0ce5f752a1d2569af7fb62a49acf792398
Alpha Olomi <alphaolomi@gmail.com>
2019-12-29 23:51:44 +0300

    style(backend): lint with prettier

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,16 @@
-import { GraphQLServer, PubSub } from 'graphql-yoga'
-import db from './db'
-import Query from './resolvers/Query'
-import Mutation from './resolvers/Mutation'
-import Subscription from './resolvers/Subscription'
-import User from './resolvers/User'
-import Post from './resolvers/Post'
-import Comment from './resolvers/Comment'
+import { GraphQLServer, PubSub } from "graphql-yoga";
+import db from "./db";
+import Query from "./resolvers/Query";
+import Mutation from "./resolvers/Mutation";
+import Subscription from "./resolvers/Subscription";
+import User from "./resolvers/User";
+import Post from "./resolvers/Post";
+import Comment from "./resolvers/Comment";
 
-const pubsub = new PubSub()
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
-  typeDefs: './src/schema.graphql',
+  typeDefs: "./src/schema.graphql",
   resolvers: {
     Query,
     Mutation,
@@ -23,8 +23,8 @@ const server = new GraphQLServer({
     db,
     pubsub
   }
-})
+});
 
 server.start({ port: process.env.PORT | 4000 }, () => {
-  console.log(`The server is up on port ${process.env.PORT | 4000}!`)
-})
+  console.log(`The server is up on port ${process.env.PORT | 4000}!`);
+});
