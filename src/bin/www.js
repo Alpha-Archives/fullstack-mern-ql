639dd2e4d8fdb957a7344cd3952cc208b82b5a4e
Alpha Olomi <alphaolomi@gmail.com>
2020-12-27 11:05:14 +0300

    update backend deps

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,5 +1,19 @@
 import app from "../app";
 
-app.start({ port: process.env.PORT | 4000 }, () => {
-  console.log(`The server is up on port ${process.env.PORT | 4000}!`);
-});
+
+const options = {
+  port: 8000,
+  endpoint: '/graphql',
+  subscriptions: '/subscriptions',
+  playground: '/playground',
+}
+
+app.start(options, ({ port }) =>
+  console.log(
+    `Server started, listening on port ${port} for incoming requests.`,
+  ),
+)
+
+// app.start({ port: process.env.PORT | 4000 }, () => {
+//   console.log(`The server is up on port ${process.env.PORT | 4000}!`);
+// });
